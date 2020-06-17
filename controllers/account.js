61782237afa9fa73fd13ba4caa7ac04ab52d20a1
AbdulHannanButt <hannanbutt1995@gmail.com>
2020-06-17 12:40:58 +0500

    done work

--- controllers/account.js
+++ controllers/account.js
@@ -2,8 +2,29 @@ const bcrypt = require('bcryptjs');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Account = require('../models/account');
-const SystemAdmin = require('../models/systemAdmin');
+
 exports.getAccount = asyncHandler(async (req, res) => {
   const account = await Account.find().populate('mrId').populate('vendorId');
+  res.status(200).json({ success: true, data: account });
+});
+exports.getAccountById = asyncHandler(async (req, res) => {
+  const account = await Account.findOne({_id:req.params._id})
+  .populate('mrId').populate('vendorId')
+  res.status(200).json({ success: true, data: account });
+});
+
+exports.updateAccount = asyncHandler(async (req, res, next) => {
+  const { _id } = req.body;
+
+  let account = await Account.findById(_id);
+
+  if (!account) {
+    return next(
+      new ErrorResponse(`Staff type not found with id of ${_id}`, 404)
+    );
+  }
+
+  account = await Account.updateOne({ _id: _id }, req.body);
+
   res.status(200).json({ success: true, data: account });
 });
