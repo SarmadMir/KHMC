fc288f4fbfdb1af6a8dca7374f5c26a4c68af60f
AbdulHannanButt <hannanbutt1995@gmail.com>
2020-06-22 11:14:04 +0500

    receiveItem change

--- controllers/receiveItem.js
+++ controllers/receiveItem.js
@@ -4,6 +4,7 @@ const asyncHandler = require('../middleware/async');
 const ReceiveItem = require('../models/receiveItem');
 const MaterialReceiving = require('../models/materialReceiving');
 const WhInventory = require('../models/warehouseInventory');
+const PurchaseRequest = require('../models/purchaseRequest');
 const Account = require('../models/account');
 exports.getReceiveItems = asyncHandler(async (req, res) => {
     const receiveItems = await ReceiveItem.find().populate('vendorId');
@@ -43,8 +44,9 @@ exports.addReceiveItem = asyncHandler(async (req, res) => {
         dateReceived,
         notes
     });
+    await PurchaseRequest.findOneAndUpdate({'_id': prId},{ $set: { status: 'pending_approval_from_accounts' }},{new: true});
     await WhInventory.updateOne({itemId: itemId}, { $set: { qty: currentQty+receivedQty }})
-   const mat = await MaterialReceiving.findOneAndUpdate({'_id': materialId,'prId.id':prId},{ $set: { 'prId.$.status': 'received' }},{new: true});
+    const mat = await MaterialReceiving.findOneAndUpdate({'_id': materialId,'prId.id':prId},{ $set: { 'prId.$.status': 'received' }},{new: true});
    var count = 0;
     for(let i = 0; i<mat.prId.length; i++)
     {
