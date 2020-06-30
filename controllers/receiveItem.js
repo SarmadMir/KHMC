a44d9fc811ff88887f2b24bd264e176662aa0cf3
AbdulHannanButt <hannanbutt1995@gmail.com>
2020-06-30 16:59:46 +0500

    date fix external return request

--- controllers/receiveItem.js
+++ controllers/receiveItem.js
@@ -22,23 +22,24 @@ exports.addReceiveItem = asyncHandler(async (req, res) => {
     const { itemId,currentQty, requestedQty, receivedQty, bonusQty, batchNumber,lotNumber,
         expiryDate,unit, discount, unitDiscount, discountAmount, tax, taxAmount, finalUnitPrice, subTotal, 
         discountAmount2,totalPrice, invoice, dateInvoice,dateReceived, notes,materialId,vendorId,prId,status } = req.body;
-          // var isafter = moment(req.body.dateReceived).isAfter(req.body.expiryDate);
-            // if (isafter)
-            // {
-                // await ExternalReturnRequest.create({
-                //     returnRequestNo: uuidv4(),
-                //     generatedBy:"System",
-                //     dateGenerated:req.body.dateReceived,
-                //     expiryDate:req.body.expiryDate,
-                //     itemId:req.body.itemId,
-                //     currentQty:req.body.qty,
-                //     description,
-                //     reason:"Date Expired",
-                //     reasonDetail:"Date Expired",
-                //     status:"approved",
-                //     prId:req.body.prId
-                // })
-            // }
+          var isafter = moment(req.body.dateReceived).isAfter(req.body.expiryDate);
+            if (isafter)
+            {
+                await ExternalReturnRequest.create({
+                    returnRequestNo: uuidv4(),
+                    generatedBy:"System",
+                    dateGenerated:req.body.dateReceived,
+                    expiryDate:req.body.expiryDate,
+                    itemId:req.body.itemId,
+                    currentQty:req.body.qty,
+                    description,
+                    reason:"Date Expired",
+                    reasonDetail:"Date Expired",
+                    status:"approved",
+                    prId:req.body.prId
+                })
+            }
+            if(!isafter){
         if(req.body.receivedQty>req.body.requestedQty)
         {
         var qty=req.body.receivedQty-req.body.requestedQty;    
@@ -106,9 +107,8 @@ exports.addReceiveItem = asyncHandler(async (req, res) => {
                 dateReceived,
                 notes,prId,status
             });
-        }
-        
-  
+        }  
+    }
     
     await PurchaseRequest.findOneAndUpdate({'_id': prId},{ $set: { status: 'pending_approval_from_accounts' }},{new: true});
     const mat = await MaterialReceiving.findOneAndUpdate({'_id': materialId,'prId.id':prId},{ $set: { 'prId.$.status': req.body.status }},{new: true});
