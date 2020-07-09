06d191ab55c95a1bd9a0c690f616b8f424f4b616
AbdulHannanButt <hannanbutt1995@gmail.com>
2020-07-09 16:47:21 +0500

    test

--- controllers/replenishmentRequestBU.js
+++ controllers/replenishmentRequestBU.js
@@ -1,14 +1,23 @@
 /* eslint-disable prefer-const */
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const webpush = require("web-push");
 const { v4: uuidv4 } = require('uuid');
 const ReplenishmentRequestBU = require('../models/replenishmentRequestBU');
-const WHInventory = require('../models/warehouseInventory');
 const FunctionalUnit = require('../models/functionalUnit');
 const FUInventory = require('../models/fuInventory');
 const BUInventory = require('../models/buInventory');
-const PurchaseRequest = require('../models/purchaseRequest');
-const Item = require('../models/item');
+const ReplenishmentRequest = require('../models/replenishmentRequest');
+const StaffType = require('../models/staffType');
+const User = require('../models/user')
+const Subscription = require('../models/subscriber')
+const privateVapidKey = "s92YuYXxjJ38VQhRSuayTb9yjN_KnVjgKfbpsHOLpjc";
+const publicVapidKey = "BOHtR0qVVMIA-IJEru-PbIKodcux05OzVVIJoIBKQu3Sp1mjvGkjaT-1PIzkEwAiAk6OuSCZfNGsgYkJJjOyV7k"
+webpush.setVapidDetails(
+  "mailto:hannanbutt1995@gmail.com",
+  publicVapidKey,
+  privateVapidKey
+);
 exports.getReplenishmentRequestsBU = asyncHandler(async (req, res) => {
     const replenishmentRequest = await ReplenishmentRequestBU.find().populate('buId').populate('fuId').populate('itemId');    
     res.status(200).json({ success: true, data: replenishmentRequest });
@@ -95,15 +104,76 @@ exports.updateReplenishmentRequestBU = asyncHandler(async (req, res, next) => {
         new ErrorResponse(`Replenishment Request not found with id of ${_id}`, 404)
         );
     }
-    // if(req.body.status=="complete")
-    // { 
-    //     const bui = await FunctionalUnit.findOne({buId:req.body.buId})//wrong logic change when more data
-    //     const fui = await FUInventory.findOne({itemId: req.body.itemId,fuId:bui._id})   
-    //     const bu = await BUInventory.findOne({itemId: req.body.itemId,buId:req.body.buId})
-    //     const fu = await FUInventory.findOne({itemId: req.body.itemId,_id:fui._id})
-    //     await BUInventory.findOneAndUpdate({itemId: req.body.itemId,buId:req.body.buId}, { $set: { qty: bu.qty+req.body.requestedQty }},{new:true})
-    //     await FUInventory.findOneAndUpdate({itemId: req.body.itemId}, { $set: { qty: fu.qty-req.body.requestedQty }},{new:true}).populate('itemId')   
-    // }
+    if(req.body.status=="complete")
+    { 
+        const bui = await FunctionalUnit.findOne({buId:req.body.buId})//wrong logic change when more data
+        const fui = await FUInventory.findOne({itemId: req.body.itemId,fuId:bui._id})   
+        const bu = await BUInventory.findOne({itemId: req.body.itemId,buId:req.body.buId})
+        const fu = await FUInventory.findOne({itemId: req.body.itemId,_id:fui._id})
+        await BUInventory.findOneAndUpdate({itemId: req.body.itemId,buId:req.body.buId}, { $set: { qty: bu.qty+req.body.requestedQty }},{new:true})
+        const rr = await FUInventory.findOneAndUpdate({itemId: req.body.itemId}, { $set: { qty: fu.qty-req.body.requestedQty }},{new:true}).populate('itemId')   
+        if(rr.qty<=rr.itemId.reorderLevel)
+        {
+            await ReplenishmentRequest.create({
+                requestNo: uuidv4(),
+                generated:'System',
+                generatedBy:'System',
+                reason:'Item quantity in Functional Unit is low then reorder level',
+                fuId:fu._id,//Wrong logic should be dynamic
+                comments:'System generated Replenishment Request',
+                currentQty:rr.qty,
+                requestedQty:rr.itemId.maximumLevel-rr.qty,
+                description:'System generated Replenishment Request',
+                status: 'to_do',
+                secondStatus:'to_do',
+              });
+            const payload = JSON.stringify({ title: "Replenishment Request Generated",message:"Kindly check system generated replenishment request" });
+            const type = await StaffType.findOne({type:"FU Incharge"})
+            const user = await User.find({staffTypeId:type._id})
+            for(var i = 0; i<user.length; i++ )
+            {
+            Subscription.find({user:user[i]._id}, (err, subscriptions) => {
+              if (err) {
+                console.error(`Error occurred while getting subscriptions`);
+                res.status(500).json({
+                  error: 'Technical error occurred',
+                });
+              } else {
+                let parallelSubscriptionCalls = subscriptions.map((subscription) => {
+                  return new Promise((resolve, reject) => {
+                    const pushSubscription = {
+                      endpoint: subscription.endpoint,
+                      keys: {
+                        p256dh: subscription.keys.p256dh,
+                        auth: subscription.keys.auth,
+                      },
+                    };
+                    const pushPayload = payload;
+                    webpush
+                      .sendNotification(pushSubscription, pushPayload)
+                      .then((value) => {
+                        resolve({
+                          status: true,
+                          endpoint: subscription.endpoint,
+                          data: value,
+                        });
+                      })
+                      .catch((err) => {
+                        reject({
+                          status: false,
+                          endpoint: subscription.endpoint,
+                          data: err,
+                        });
+                      });
+                  });
+                });
+              }
+            });
+          }
+          const rr2 = await ReplenishmentRequest.find().populate('fuId').populate('itemId').populate('approvedBy')
+          globalVariable.io.emit("get_data", rr2)
+    }
+    }
     replenishmentRequest = await ReplenishmentRequestBU.findOneAndUpdate({_id: _id}, req.body,{new:true});
     res.status(200).json({ success: true, data: replenishmentRequest });
 });
